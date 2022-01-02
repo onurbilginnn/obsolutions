ee53f844a716a26e35ad94903e8c2e02f0815894
onurbilginnn <onurbilginnn@gmail.com>
2022-01-02 16:16:59 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from 'react';
 import Layout from './hoc/Layout';
 
 const App = () => {
-  console.log(process.env.REACT_APP_MAIL_URL)
-  console.log(process.env.REACT_APP_API_KEY)
   return <Layout />;
 }
 
