27ef4aa4c69d1816d22803571581782d490575cd
onurbilginnn <onurbilginnn@gmail.com>
2022-01-02 16:34:46 +0300

    Updates

---
+++ precache-manifest.74616710a4100d01dbfcbf897dda5127.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d58a331d92850e5a4e72215b150d1e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6734652c9aff27c960ef",
+    "url": "/static/css/main.aa21e535.chunk.css"
+  },
+  {
+    "revision": "a7c538d903d6eb3a0466",
+    "url": "/static/js/2.9d940670.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9d940670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6734652c9aff27c960ef",
+    "url": "/static/js/main.f74dcf8d.chunk.js"
+  },
+  {
+    "revision": "d944ec1613c989f3e80f",
+    "url": "/static/js/runtime-main.a975f0d6.js"
+  },
+  {
+    "revision": "004d50ea153d33283d6d5942ebbf76bd",
+    "url": "/static/media/AWSCPP.004d50ea.png"
+  },
+  {
+    "revision": "fa46256c26fae663477d967dc3874ad0",
+    "url": "/static/media/AWSDev.fa46256c.png"
+  },
+  {
+    "revision": "b8fc0794730dd56dff86ee374b38859b",
+    "url": "/static/media/AWSSAAS.b8fc0794.png"
+  },
+  {
+    "revision": "1be3e0aaeb2bbd1985615a49da7f2eaf",
+    "url": "/static/media/Electrolize-Regular.1be3e0aa.ttf"
+  },
+  {
+    "revision": "b9b2108380fd765dc649f228268488a2",
+    "url": "/static/media/OB-Dark-2.b9b21083.jpg"
+  },
+  {
+    "revision": "155354b885e87ff88fc15d7d65a65847",
+    "url": "/static/media/OB-Dark.155354b8.jpg"
+  },
+  {
+    "revision": "03c11f6b0c0f707075d6483a78824c60",
+    "url": "/static/media/SourceCodePro-Bold.03c11f6b.ttf"
+  },
+  {
+    "revision": "c088801620ae4d69924da74e879170a9",
+    "url": "/static/media/SourceCodePro-Italic.c0888016.ttf"
+  },
+  {
+    "revision": "b484b32fcec981a533e3b9694953103b",
+    "url": "/static/media/SourceCodePro-Regular.b484b32f.ttf"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/static/media/javascript.e8c27f6b.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/static/media/react.70f87bba.png"
+  },
+  {
+    "revision": "efd8420d866a2dc6daabcbceef731f01",
+    "url": "/static/media/small-home-bg.efd8420d.png"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Layout from './hoc/Layout';
-
-const App = () => {
-  return <Layout />;
-}
-
-export default App;
